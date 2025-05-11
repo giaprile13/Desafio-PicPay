440c0fefb14ab236c51a85f3c70ee2d92a0594dd
giaprile13 <giaprile77@gmail.com>
2025-05-11 12:35:00 -0300

    chore: implemented balance and userType properties

--- src/main/java/br/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/br/com/picpaysimplificado/domain/user/User.java
@@ -3,9 +3,12 @@ package br.com.picpaysimplificado.domain.user;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity(name = "users")
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -13,14 +16,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String firstName;
     private String lastName;
     @Column(unique = true)
-    private String doucment;
+    private String document;
     @Column(unique = true)
     private String email;
     private String password;
-
-
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
 }
